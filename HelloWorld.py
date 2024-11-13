b218719fcc538d860ba885b821e7122e479568a0
Suraj P <psuraj4327@gmail.com>
2024-11-13 10:55:35 +0530

    First code in python - print() function, Python introduction - What is an interpreted language, What is byte code, What is Scripts?

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+"""Why "Hello, world!" phrase? 
+    The phrase Hello, world! is a special phrase in programming. In 1972 Brian Kernighan, 
+    a computer scientist, wrote a tutorial for the B programming language using this phrase.
+"""
+
+# First code in python
+print("Hello, world!")
+
+# What does print() mean?
+"""In python, print() is a in-built function used to display to the console."""
